252585bb30b402bd464c817a89564e324c4127d2
Jerome Reviron <jrinstitut63@hotmail.fr>
2024-01-27 16:09:57 +0100

    Mise en page de l interface fromage_UI.py

--- fromage_ui.py
+++ fromage_ui.py
@@ -1,6 +1,3 @@
-"""
-Module d'interface utilisateur pour afficher des informations sur les fromages à l'aide de Tkinter.
-"""
 from imports import tk, messagebox, Figure, FigureCanvasTkAgg
 from scrapping_fromage import FromageETL
 
@@ -8,54 +5,45 @@ class FromageUI:
     """
     Classe représentant l'interface utilisateur pour afficher des informations sur les fromages.
     """
+    URL_FROMAGE = "https://www.laboitedufromager.com/liste-des-fromages-par-ordre-alphabetique/"
+    DB_NAME = "fromages_bdd.sqlite"
+    TABLE_NAME = "fromages_table"
+
     def __init__(self, master):
         """
         Initialisation de l'interface graphique de la classe FromageUI.
-
-        Parameters:
-        - master (Tk): Fenêtre principale de l'application.
-
-        Cette méthode crée une interface utilisateur pour la gestion des fromages,
-        comprenant un bouton pour mettre à jourla base de données (BDD),
-        et un diagramme en camembert pour afficher visuellement,
-        les informations sur les fromages.
-
-        Usage:
-        fromage_ui = FromageUI(master)
         """
         self.master = master
         master.title("Interface Fromage")
 
+        # Ouvrir la fenêtre en plein écran
+        master.attributes('-fullscreen', True)
+
+        # Cadre principal
+        self.main_frame = tk.Frame(master)
+        self.main_frame.pack()
+
         # Bouton pour mettre à jour la BDD
-        self.update_button = tk.Button(master, text="Mettre à jour la BDD",
-            command=self.update_database)
+        self.update_button = tk.Button(self.main_frame, text="Mettre à jour la BDD", command=self.update_database)
         self.update_button.pack()
 
         # Diagramme en camembert
-        self.fig = Figure(figsize=(5, 5), dpi=100)
-        self.canvas = FigureCanvasTkAgg(self.fig, master=master)
+        self.fig = Figure(figsize=(10, 5), dpi=100)
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.main_frame)
         self.canvas.get_tk_widget().pack()
+
+        # Bouton pour fermer la fenêtre
+        self.close_button = tk.Button(self.main_frame, text="Fermer", command=self.close_window)
+        self.close_button.pack()
 
     def update_database(self):
         """
-        Met à jour la base de données (BDD) en extrayant,
-        transformant et chargeant les données des fromages.
-
-        Cette méthode utilise la classe FromageETL pour
-        extraire les données depuis une source en ligne, 
-        les transformer en un format approprié, 
-        et les charger dans une base de données SQLite. 
-        Elle affiche ensuite une boîte de dialogue
-        informant l'utilisateur du succès de la mise à jour.
-
-        Usage:
-        fromage_ui.update_database()
+        Met à jour la base de données (BDD) en extrayant, transformant et chargeant les données des fromages.
         """
-        # Appeler la méthode d'ETL pour mettre à jour la BDD
-        etl = FromageETL(url="https://www.laboitedufromager.com/liste-des-fromages-par-ordre-alphabetique/")
+        etl = FromageETL(url=self.URL_FROMAGE)
         etl.extract()
         etl.transform()
-        data = etl.load("fromages_bdd.sqlite", "fromages_table")
+        data = etl.load(self.DB_NAME, self.TABLE_NAME)
         messagebox.showinfo("Mise à jour", "La base de données a été mise à jour avec succès.")
 
         # Mettre à jour le diagramme en camembert
@@ -63,23 +51,8 @@ class FromageUI:
 
     def update_pie_chart(self, data):
         """
-        Met à jour le diagramme en camembert 
-        avec les ratios de fromages par famille.
-
-        Cette méthode prend un DataFrame de données
-        sur les fromages en paramètre, 
-        calcule le ratio de chaque famille de fromages, 
-        groupe les familles dont le ratio est inférieur à 5%
-        sous la catégorie "Autres", 
-        et met à jour le diagramme en camembert en conséquence.
-
-        Args:
-            data (pd.DataFrame): Le DataFrame contenant les données.
-
-        Usage:
-        fromage_ui.update_pie_chart(data)
+        Met à jour les deux diagrammes en camembert avec les ratios de fromages par famille.
         """
-        # Calculer le ratio de fromage par famille
         ratio = data['fromage_familles'].value_counts(normalize=True) * 100
 
         # Regrouper les données inférieures à 5% dans "Autres"
@@ -88,21 +61,52 @@ class FromageUI:
         mask = mask.reindex(ratio.index, fill_value=False)
         ratio = ratio[~mask]
 
-        # Créer le diagramme en camembert
+        # Créer les deux diagrammes en camembert côte à côte
         self.fig.clear()
-        ax = self.fig.add_subplot(111)
-        ax.pie(ratio, labels=ratio.index, autopct='%1.1f%%')
-        self.canvas.draw()
+
+        # Créer le premier diagramme en camembert
+        ax1 = self.fig.add_axes([0, 0, 0.4, 1])  # [left, bottom, width, height]
+        ax1.pie(ratio, labels=ratio.index, autopct='%1.1f%%')
+        ax1.set_title("100% de la BDD")
+
+        # Ajouter une légende pour le premier camembert
+        legend_labels1 = [f"{label} : {ratio[label]:.1f}%" for label in ratio.index]
+        legend1 = ax1.legend(legend_labels1, title="% BDD des 'Familles' > 5%")
+
+        # Ajuster la position de la légende par rapport au camembert
+        legend1.set_bbox_to_anchor((0.8, 0.82))  # Coordonnées relatives à l'axe du camembert
+        legend1.get_title().set_fontsize('10')
+        # Ajuster la taille de la légende (ajuster toutes les étiquettes)
+        for text in legend1.get_texts():
+            text.set_fontsize('8')
+
 
         # Créer un deuxième diagramme en camembert pour "Autres"
         if 'Autres' in ratio:
             other_data = data['fromage_familles'].value_counts(normalize=True)[mask] * 100
-            other_fig = Figure(figsize=(5, 5), dpi=100)
-            other_canvas = FigureCanvasTkAgg(other_fig, master=self.master)
-            other_canvas.get_tk_widget().pack()
-            other_ax = other_fig.add_subplot(111)
-            other_ax.pie(other_data, labels=other_data.index, autopct='%1.1f%%')
-            other_canvas.draw()
+            ax2 = self.fig.add_axes([0.55, 0, 0.3, 1]) 
+            ax2.pie(other_data, labels=other_data.index, autopct='%1.1f%%')
+            ax2.set_title("100% des Autres", loc='center', pad=40)
+
+            # Ajouter une légende pour le deuxième camembert
+            legend_labels2 = [f"{label} : {other_data[label]:.1f}%" for label in other_data.index]
+            legend2 = ax2.legend(legend_labels2, title="% BDD des 'Autres'")
+
+            # Ajuster la position de la légende
+            legend2.set_bbox_to_anchor((0.8, 0.80))
+            legend2.get_title().set_fontsize('10')
+            
+            # Ajuster la taille des étiquettes
+            for text in legend2.get_texts():
+                text.set_fontsize('8')
+
+        self.canvas.draw()
+
+    def close_window(self):
+            """
+            Fonction pour fermer la fenêtre.
+            """
+            self.master.destroy()
 
 if __name__ == "__main__":
     root = tk.Tk()
