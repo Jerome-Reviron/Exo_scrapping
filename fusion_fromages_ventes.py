917c71a974e4f9c68ef54e5d5f161224323b19e4
Jerome Reviron <jrinstitut63@hotmail.fr>
2024-02-06 12:02:28 +0100

    amélioration des fusions csv

--- fusion_fromages_ventes.py
+++ fusion_fromages_ventes.py
@@ -9,12 +9,18 @@ output_merged_csv_path = r'C:\Users\HB\Desktop\Git Exo_Scrapping\csv\fusion_from
 df_fromages = pd.read_csv(fromages_csv_path, delimiter=',')
 df_cheeses_sales = pd.read_csv(cheeses_sales_csv_path, delimiter=';')
 
+# Renommer la colonne 'cheeses' en 'fromage_names' dans le DataFrame df_cheeses_sales
+df_cheeses_sales = df_cheeses_sales.rename(columns={'cheeses': 'fromage_names'})
+
 # Afficher les noms de colonnes pour vérification
 print("Colonnes dans df_fromages:", df_fromages.columns)
 print("Colonnes dans df_cheeses_sales:", df_cheeses_sales.columns)
 
 # Fusionner les DataFrames en utilisant la colonne 'fromage_names' comme clé
-merged_df = pd.merge(df_fromages, df_cheeses_sales, left_on='fromage_names', right_on='cheeses', how='left')
+merged_df = pd.merge(df_fromages, df_cheeses_sales, on='fromage_names', how='left')
+
+# Renommer les colonnes 'dates' et 'quantities' dans le DataFrame fusionné
+merged_df = merged_df.rename(columns={'dates': 'dates de ventes', 'quantities': 'quantités vendues'})
 
 # Enregistrer le DataFrame fusionné dans un nouveau fichier CSV
 merged_df.to_csv(output_merged_csv_path, index=False, encoding='utf-8', sep=',')
